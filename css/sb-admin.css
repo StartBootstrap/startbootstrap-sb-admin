8c22a7244e151b6a3ca3eaeba901e469f70c2675
David Miller <hello@davidmiller.io>
2017-08-21 19:49:27 -0400

    update package.json and run gulp

--- css/sb-admin.css
+++ css/sb-admin.css
@@ -1,5 +1,5 @@
 /*!
- * Start Bootstrap - SB Admin v4.0.0-beta (http://startbootstrap.com/template-overviews/sb-admin)
+ * Start Bootstrap - SB Admin v4.0.0-beta (https://startbootstrap.com/template-overviews/sb-admin)
  * Copyright 2013-2017 Start Bootstrap
  * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-sb-admin/blob/master/LICENSE)
  */
