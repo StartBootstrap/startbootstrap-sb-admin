ea3934617bdd103a779b6960f9855da444944675
David Miller <hello@davidmiller.io>
2017-10-20 18:54:45 -0400

    remove popper from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -87,9 +87,6 @@ gulp.task('copy', function() {
   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
     .pipe(gulp.dest('vendor/jquery'))
 
-  gulp.src(['node_modules/popper.js/dist/umd/popper.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-    .pipe(gulp.dest('vendor/popper'))
-
   gulp.src(['node_modules/jquery.easing/*.js'])
     .pipe(gulp.dest('vendor/jquery-easing'))
 
