343cd0c62ecdd45c3d3c83db852c0c5ade2dbd99
David Miller <hello@davidmiller.io>
2018-07-25 18:27:38 -0400

    SB Admin v5.0.0 Update
    SB Admin version 5 is now here with a reworked menu structure, which is reliable in both fixed and static formats, Bootstrap 4.1.3, and Font Awesome 5. Development will continue to improve this theme from here.

--- js/sb-admin.js
+++ js/sb-admin.js
@@ -1,28 +1,23 @@
 (function($) {
   "use strict"; // Start of use strict
-  // Configure tooltips for collapsed side navigation
-  $('.navbar-sidenav [data-toggle="tooltip"]').tooltip({
-    template: '<div class="tooltip navbar-sidenav-tooltip" role="tooltip" style="pointer-events: none;"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
-  })
+
   // Toggle the side navigation
-  $("#sidenavToggler").click(function(e) {
+  $("#sidebarToggle").click(function(e) {
     e.preventDefault();
-    $("body").toggleClass("sidenav-toggled");
-    $(".navbar-sidenav .nav-link-collapse").addClass("collapsed");
-    $(".navbar-sidenav .sidenav-second-level, .navbar-sidenav .sidenav-third-level").removeClass("show");
+    $("body").toggleClass("sidebar-toggled");
+    $(".sidebar").toggleClass("toggled");
   });
-  // Force the toggled class to be removed when a collapsible nav link is clicked
-  $(".navbar-sidenav .nav-link-collapse").click(function(e) {
-    e.preventDefault();
-    $("body").removeClass("sidenav-toggled");
-  });
+
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-  $('body.fixed-nav .navbar-sidenav, body.fixed-nav .sidenav-toggler, body.fixed-nav .navbar-collapse').on('mousewheel DOMMouseScroll', function(e) {
-    var e0 = e.originalEvent,
-      delta = e0.wheelDelta || -e0.detail;
-    this.scrollTop += (delta < 0 ? 1 : -1) * 30;
-    e.preventDefault();
+  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+    if ($window.width() > 768) {
+      var e0 = e.originalEvent,
+        delta = e0.wheelDelta || -e0.detail;
+      this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+      e.preventDefault();
+    }
   });
+
   // Scroll to top button appear
   $(document).scroll(function() {
     var scrollDistance = $(this).scrollTop();
@@ -32,8 +27,7 @@
       $('.scroll-to-top').fadeOut();
     }
   });
-  // Configure tooltips globally
-  $('[data-toggle="tooltip"]').tooltip()
+
   // Smooth scrolling using jQuery easing
   $(document).on('click', 'a.scroll-to-top', function(event) {
     var $anchor = $(this);
@@ -42,4 +36,5 @@
     }, 1000, 'easeInOutExpo');
     event.preventDefault();
   });
+
 })(jQuery); // End of use strict
