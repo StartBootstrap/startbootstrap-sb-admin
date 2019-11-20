c782a68b2490ffa94cd1a389918860225a5e2f0b
Sam Artioli <initplatform@gmail.com>
2019-11-19 20:37:13 -0500

    prettier and travis

--- scripts/render-pug.js
+++ scripts/render-pug.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const pug = require('pug');
 const sh = require('shelljs');
+const prettier = require('prettier');
 
 module.exports = function renderPug(filePath) {
     const destPath = filePath.replace(/src\/pug\/\pages\//, 'dist/').replace(/\.pug$/, '.html');
@@ -10,12 +11,21 @@ module.exports = function renderPug(filePath) {
     const html = pug.renderFile(filePath, {
         doctype: 'html',
         filename: filePath,
-        pretty: true,
     });
 
     const destPathDirname = path.dirname(destPath);
     if (!sh.test('-e', destPathDirname)) {
         sh.mkdir('-p', destPathDirname);
     }
-    fs.writeFileSync(destPath, html);
+
+    const prettified = prettier.format(html, {
+        printWidth: 1000,
+        tabWidth: 4,
+        singleQuote: true,
+        proseWrap: 'preserve',
+        endOfLine: 'lf',
+        parser: 'html'
+    });
+
+    fs.writeFileSync(destPath, prettified);
 };
