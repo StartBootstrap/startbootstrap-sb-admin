373213b692542f60d16da9773d97cd3b900ea43c
Sam Artioli <initplatform@gmail.com>
2019-11-19 20:47:34 -0500

    autoprefixer

--- scripts/render-scss.js
+++ scripts/render-scss.js
@@ -1,16 +1,18 @@
 'use strict';
+const autoprefixer = require('autoprefixer')
 const fs = require('fs');
-const path = require('path');
-const sh = require('shelljs');
-const sass = require('sass');
 const packageJSON = require('../package.json');
+const path = require('path');
+const postcss = require('postcss')
+const sass = require('sass');
+const sh = require('shelljs');
 
 const stylesPath = path.resolve(path.dirname(__filename), '../src/scss/styles.scss');
 const destPath = path.resolve(path.dirname(__filename), '../dist/css/styles.css');
 
 module.exports = function renderSCSS() {
     
-    const result = sass.renderSync({
+    const results = sass.renderSync({
         // file: stylesPath,
         data: entryPoint,
         includePaths: [
@@ -24,7 +26,13 @@ module.exports = function renderSCSS() {
     if (!sh.test('-e', destPathDirname)) {
         sh.mkdir('-p', destPathDirname);
     }
-    fs.writeFileSync(destPath, result.css.toString());
+
+    postcss([ autoprefixer ]).process(results.css, {from: 'styles.css', to: 'styles.css'}).then(result => {
+        result.warnings().forEach(warn => {
+            console.warn(warn.toString())
+        })
+        fs.writeFileSync(destPath, result.css.toString());
+    })
 
 };
 
