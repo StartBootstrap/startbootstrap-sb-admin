f4b97a6b5f1c2d2d7431750a6d8804f26320e519
David Miller <hello@davidmiller.io>
2020-06-18 18:02:25 -0400

    update dependencies and run build

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-    * Start Bootstrap - SB Admin v6.0.0 (https://startbootstrap.com/templates/sb-admin)
+    * Start Bootstrap - SB Admin v6.0.1 (https://startbootstrap.com/templates/sb-admin)
     * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-sb-admin/blob/master/LICENSE)
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
     */
     (function($) {
     "use strict";
