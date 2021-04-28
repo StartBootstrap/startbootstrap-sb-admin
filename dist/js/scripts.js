3efbbcc864cdb5cefd2b288135cc7b1709e2436a
David Miller <hello@davidmiller.io>
2021-04-28 15:50:47 -0400

    Bootstrap 4.6.0 and dependencies update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-    * Start Bootstrap - SB Admin v6.0.2 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2020 Start Bootstrap
+    * Start Bootstrap - SB Admin v6.0.3 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2021 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
     */
     (function($) {
