db9086fefd4a89ab5ea4574d0daa1fe0c5243ae8
David Miller <hello@davidmiller.io>
2021-06-29 13:19:36 -0400

    Bootstrap 5.0.2 update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-    * Start Bootstrap - SB Admin v7.0.1 (https://startbootstrap.com/template/sb-admin)
+    * Start Bootstrap - SB Admin v7.0.2 (https://startbootstrap.com/template/sb-admin)
     * Copyright 2013-2021 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
     */
