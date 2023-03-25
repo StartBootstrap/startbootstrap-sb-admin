3dbc7471f4a300e43748d16cbcfbda3f179459b2
David Miller <hello@davidmiller.io>
2023-03-25 18:09:39 -0400

    dev dependency update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-    * Start Bootstrap - SB Admin v7.0.6 (https://startbootstrap.com/template/sb-admin)
+    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
     * Copyright 2013-2023 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
     */
